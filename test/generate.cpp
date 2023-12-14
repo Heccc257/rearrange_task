f8cd0d4706c0df7f1145bcb34d1d77561a8a6199
hch <2762312941@qq.com>
2023-12-15 00:53:01 +0800

    .

--- test/generate.cpp
+++ test/generate.cpp
@@ -10,7 +10,7 @@ using namespace std;
 uint64_t buffer[1024];
 int main() {
     // int fd = open("test.dat", O_WRONLY | O_CREAT);
-    ofstream outFile("test2.dat", std::ios::out);
+    ofstream outFile("test.dat", std::ios::out);
     if (!outFile.is_open()) {
         std::cerr << "can't open file\n";
         return 0;
@@ -20,11 +20,11 @@ int main() {
         buffer[i] = 1ll * rand() * rand();
     
     for (int i = 0; i < 9; i++) {
-        buffer[0] ^= (1<<i);
+        // buffer[0] ^= (1<<i);
         outFile.write(reinterpret_cast<char*>(buffer), sizeof(buffer));
     }
-    memset(buffer, 1, sizeof(buffer));
-    outFile.write(reinterpret_cast<char*>(buffer), sizeof(buffer));
+    // memset(buffer, 1, sizeof(buffer));
+    // outFile.write(reinterpret_cast<char*>(buffer), sizeof(buffer));
     // for (int i = 0; i < 9; i++) {
     //     memset(buffer, 0, sizeof(buffer));
     //     for (int j = 0; j < 1024; j++)
